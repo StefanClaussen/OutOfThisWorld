f4e7e3edacb21d8eeef7401cc26c8cc3f24c5448
StefenClausen <stefanclaussen@me.com>
2014-04-02 11:33:12 +0100

    Initial Commit

---
+++ Out of this World/STCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STCAppDelegate.h
+//  Out of this World
+//
+//  Created by Stefan Claussen on 02/04/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
