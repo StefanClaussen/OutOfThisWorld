6f5bbb44bf40062ed2804d8ef318d32ef4963248
StefenClausen <stefanclaussen@me.com>
2014-05-06 14:35:15 +0100

    Add AddSpaceObjectView and view controller.  Set up the view and make the connections.

---
+++ Out of this World/STCAddSpaceObjectViewController.h
@@ -0,0 +1,23 @@
+//
+//  STCAddSpaceObjectViewController.h
+//  Out of this World
+//
+//  Created by Stefan Claussen on 06/05/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCAddSpaceObjectViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *nameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *nicknameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *diameterTextField;
+@property (strong, nonatomic) IBOutlet UITextField *temperatureTextField;
+@property (strong, nonatomic) IBOutlet UITextField *numberOfMoonsTextField;
+@property (strong, nonatomic) IBOutlet UITextField *interestingFactTextField;
+
+- (IBAction)cancelButtonPressed:(UIButton *)sender;
+- (IBAction)addButtonPressed:(UIButton *)sender;
+
+@end
